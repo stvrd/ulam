1748a8b35b8508f18522d45f0162144daf18d49d
Стив Риедо <33114869+stvrd@users.noreply.github.com>
2019-02-19 16:52:41 +0100

    Update EUC.R

--- EUC.R
+++ EUC.R
@@ -1,5 +1,6 @@
-# Euclids algorithm
 
+
+# Euclids algorithm for unary digits
 library(tidyverse)
 
 EUC <- function(s_i=c(0,0)){
@@ -31,6 +32,9 @@ EUC <- function(s_i=c(0,0)){
 }
 EUC(c(0,0))
 
+==========================
+#UNIVERSAL TURING MACHINE
+==========================
 
 UTM <- function(tape, sleep=F){
   i <- 1
@@ -59,8 +63,15 @@ UTM(tape,sleep = 0.5)
 
 
 # Decimal 2 binary and binary 2 decimal
+# Decimal 2 binary and binary 2 decimal
 
-D2B <- function(x){
+d2b <- function(x){
+  # inverse function of b2d
+  options(scipen=999)
+  if(!is.numeric(x)) stop("x not numeric")
+  if(x <2) {return(x)
+  } else {
+    
   pow <- 2^(floor(log(x,base = 2)):0)
   binary <- vector(length = length(pow))
   binary[1] <- 1
@@ -70,14 +81,68 @@ D2B <- function(x){
     if(interm>=pow[i]) binary[i] <- 1
   }
   as.numeric(paste(binary,collapse = ""))
+  }
 }
 
-B2D <- function(x){
+b2d <- function(x){
+  # inverse function of d2b
   x <- as.numeric(strsplit(as.character(x), "")[[1]])
   if(any(x>1)) stop("x not binary")
   pow <- 2^((length(x)-1):0)
   sum(x*pow)
 }
-  
-D2B(9)
-B2D(1001)
+
+d2b(2e5)
+b2d(1001)
+
+# ==============================
+# Expanded binary notation, p.55
+# ==============================
+
+contrapt <- function(s){
+  # Inverse function of expand
+  v <- grepl("0",s)
+  if(!v[1]) v <- c(TRUE,v)
+  end <- which(v)
+  start <- lag(end)
+  v2 <- mapply( seq, start[-1], end[-1])
+  sapply(v2,function(x) length(x)-2)
+  }
+
+expand <- function(s2){
+  # Inverse function of contrapt
+  s3 <- sapply(s2,function(x) rep(TRUE,x))
+  as.numeric(c(sapply(s3,function(x) c(FALSE,x)) %>% unlist,0))
+}
+
+d2expanded <- function(v){
+  v2 <- c(strsplit(paste(sapply(v,d2b),collapse=","),"") %>% unlist(),",")
+  if(any(v2>2)) stop("floating point problem ...")
+  paste(as.numeric(sapply(v2,function(x) switch(x,"0"=0,"1"=10,","=110))),collapse = "")
+}
+                          
+# Problem with floating point:
+c(01000101101010110100011101010111100110)
+s <- as.numeric(unlist(strsplit("01000101101010110100011101010111100110","")))
+
+s2 <- contrapt(s)
+expand(s2)
+expand(c(1,3))
+
+v <- c(5,13,0,1,1,4)
+d2expanded(v)
+d2expanded(13)
+
+d2expanded(c(6,8))
+d2expanded(c(1583169,8610))
+                          
+
+# TUM on expanded binary numbers:
+
+XN2 <- tribble( ~state, ~z,      ~one,
+         0,  c(0,0,"R"),  c(1,0,"R"),
+         1,  c(0,1,"R"),  c(10,0,"R"),
+         10, c(11,1,"R"), NA,
+         11, c(0,1,"STOP"),NA)
+
+
